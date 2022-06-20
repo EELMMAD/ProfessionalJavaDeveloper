c7d69878b75dd2ce02d46175d94053660408c19f
EELMMAD <elmiramadadi@gmail.com>
2022-06-20 14:59:31 +0200

    create LearnStringsG.java

---
+++ src/main/java/se/udemy/elmira/workingWithText/People/LearnStringsG.java
@@ -0,0 +1,8 @@
+package se.udemy.elmira.workingWithText.People;
+
+public class LearnStringsG {
+    public static void main(String[] args) {
+        String myText = "Four score and seven years ago";
+        System.out.println(myText.contains("seven"));
+    }
+}
