e421f24cc178cd0faca29c03d3f62ed855dd00f8
EELMMAD <elmiramadadi@gmail.com>
2022-06-20 12:23:36 +0200

    create LearnStringsE.java

---
+++ src/main/java/se/udemy/elmira/workingWithText/People/LearnStringsE.java
@@ -0,0 +1,11 @@
+package se.udemy.elmira.workingWithText.People;
+
+public class LearnStringsE {
+    public static void main(String[] args) {
+        String myText = "Apples";
+        String middleName = "Christopher";
+
+        int length = myText.length();
+        System.out.println(myText.charAt(0));
+    }
+}
