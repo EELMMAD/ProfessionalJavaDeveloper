3f07cb3b5d0a8c6887572978a6ef97165f7f523c
EELMMAD <elmiramadadi@gmail.com>
2022-06-22 12:28:43 +0200

    create LearnStringsA.java

---
+++ src/main/java/se/udemy/elmira/workingWithText/stringConcatenation/LearnStringsA.java
@@ -0,0 +1,22 @@
+package se.udemy.elmira.workingWithText.stringConcatenation;
+
+public class LearnStringsA {
+    public static void main(String[] args) {
+        String myText = "for score and seven years ago";
+        System.out.println(myText.length());
+
+        char[] chars = myText.toCharArray();
+        int index = 1000;
+        if (index < myText.length()) {
+            System.out.println(chars[index]);
+        }
+
+        System.out.println("---------");
+        String firstText = "for";
+        String secondText = "score";
+        StringBuilder builder = new StringBuilder(firstText.length() + secondText.length())
+                .append(firstText)
+                .append(secondText);
+        System.out.println(builder.toString());
+    }
+}
