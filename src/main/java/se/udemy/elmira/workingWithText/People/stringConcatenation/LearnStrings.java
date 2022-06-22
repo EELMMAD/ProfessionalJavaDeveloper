8230930bba91e2614d0116917b743ae17c08bb90
EELMMAD <elmiramadadi@gmail.com>
2022-06-22 11:49:03 +0200

    Add StringBuilder and StringBuffer

--- src/main/java/se/udemy/elmira/workingWithText/People/stringConcatenation/LearnStrings.java
+++ src/main/java/se/udemy/elmira/workingWithText/People/stringConcatenation/LearnStrings.java
@@ -8,5 +8,29 @@ public class LearnStrings {
         System.out.println("------------");
         System.out.println(text1.concat(text2));
         System.out.println("my string literal".concat("second string literal"));
+
+        System.out.println("------------");
+        String finalString = new StringBuilder()
+                //String finalString = new StringBuilder(text1.length() + text2.length() + 1)
+                .append(text1)
+                .append(" ")
+                .append(text2)
+                .toString();
+        System.out.println(finalString);
+
+        System.out.println("------------");
+        String otherFinalString = new StringBuffer()       //trade safe
+                .append(text1)
+                .append(" ")
+                .append(text2)
+                .toString();
+        System.out.println(otherFinalString);
+
+        System.out.println("------------");
+        System.out.format("%s %s \n", text1, text2);
+        String oneMoreFinalString = String.format("%s %s \n", text1, text2);
+        System.out.println(oneMoreFinalString);
+
+
     }
 }
