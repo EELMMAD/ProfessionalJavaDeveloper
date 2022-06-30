288a62b0dcffff0ad780c5874705c6a0add2d6e3
EELMMAD <elmiramadadi@gmail.com>
2022-06-30 21:53:03 +0200

    create LearningStringsD.java

---
+++ src/main/java/se/udemy/elmira/workingWithText/stringConcatenation/LearningStringsD.java
@@ -0,0 +1,19 @@
+package se.udemy.elmira.workingWithText.stringConcatenation;
+
+public class LearningStringsD {
+    public static void main(String[] args) {
+        String fileName = "myFile.txt";
+        System.out.println(fileName.endsWith("txt")); //endsWith()--> returns boolean
+        System.out.println(fileName.endsWith("doc"));
+
+        String fileName1 = "    file001.txt";  //space matters
+       // String fileName1 = "    file001.txt".strip();
+        System.out.println(fileName1.startsWith("file"));
+        //System.out.println(fileName1.strip().startsWith("file"));
+
+       // String strippedString = fileName.strip();
+       // System.out.println(strippedString.startsWith("file"));
+
+        System.out.println(fileName1.startsWith("az"));
+    }
+}
