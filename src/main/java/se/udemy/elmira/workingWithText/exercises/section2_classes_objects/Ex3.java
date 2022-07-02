8d1832d9d678616d9e297db503e7a12629def44e
EELMMAD <elmiramadadi@gmail.com>
2022-07-02 19:10:27 +0200

    create Ex3.java

---
+++ src/main/java/se/udemy/elmira/workingWithText/exercises/section2_classes_objects/Ex3.java
@@ -0,0 +1,10 @@
+package se.udemy.elmira.workingWithText.exercises.section2_classes_objects;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        /*
+        3.Create an array and initialize it with numbers, 1-10.
+         */
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+    }
+}
