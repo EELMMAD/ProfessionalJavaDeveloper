966c8b55d292888b916c2b3af9f7d7f81217c38d
EELMMAD <elmiramadadi@gmail.com>
2022-07-05 23:28:49 +0200

    create  Ex3.java.

---
+++ src/main/java/se/udemy/elmira/workingWithText/exercises/section3_text/Ex3.java
@@ -0,0 +1,18 @@
+package se.udemy.elmira.workingWithText.exercises.section3_text;
+
+import java.util.Scanner;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        /*
+         How can you make sure when people enter text into a program, there are no unintended
+          spaces at the beginning or end of the text?
+         */
+
+
+       // Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+       // String userInput = scanner.nextLine().strip() // important part here
+        //System.out.printf("'%s'%n", userInput);
+    }
+}
