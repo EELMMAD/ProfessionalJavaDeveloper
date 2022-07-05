b8b4e596a15f4087eb0218fc7726a8879d7b7553
EELMMAD <elmiramadadi@gmail.com>
2022-07-05 23:09:30 +0200

    create  Ex2.java.

---
+++ src/main/java/se/udemy/elmira/workingWithText/exercises/section3_text/Ex2.java
@@ -0,0 +1,13 @@
+package se.udemy.elmira.workingWithText.exercises.section3_text;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        /*
+        2. Write code that replaces the word “cat” with the word “dog” wherever it shows up in a
+           sentence.
+         */
+
+        String sentence = "I love to play with cats because cats are really fun.";
+        System.out.println(sentence.replaceAll("cat", "dog"));
+    }
+}
