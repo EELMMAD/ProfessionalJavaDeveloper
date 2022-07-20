d5e6477d791aed06d7114178d00a0df38db5674d
EELMMAD <elmiramadadi@gmail.com>
2022-07-20 17:42:27 +0200

    create NumberStuff.1java double, float

---
+++ src/main/java/se/udemy/elmira/Numbers/NumberStuff1.java
@@ -0,0 +1,13 @@
+package se.udemy.elmira.Numbers;
+
+public class NumberStuff1 {
+    public static void main(String[] args) {
+        double num1 = 2.15;
+        double num2 = 1.10;
+        System.out.println(num1 - num2);
+
+        float num3 = 2.15f;
+        float num4 = 1.10f;
+        System.out.println(num3 - num4);
+    }
+}
