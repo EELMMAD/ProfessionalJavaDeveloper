e3743c16d532c332dc1efbd6eafbe23b51ae14ba
EELMMAD <elmiramadadi@gmail.com>
2022-07-20 17:33:06 +0200

    create NumberStuff.java primitive types

---
+++ src/main/java/se/udemy/elmira/Numbers/NumberStuff.java
@@ -0,0 +1,15 @@
+package se.udemy.elmira.Numbers;
+
+public class NumberStuff {
+    public static void main(String[] args) {
+        byte myByte = 127;
+        boolean myFlag = true;   //0 -> false      1 -> true
+        short myBigShort = 32767;
+        char myChar = '갱'; //gang
+        char myA = 'A';  //65
+        char biggerShort = 64000;
+        double myDouble = 3.141592;
+        float myFloat = 3.141592f;
+        System.out.println(myByte);
+    }
+}
