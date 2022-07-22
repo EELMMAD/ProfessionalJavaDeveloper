9add64ceb074eb69cf92002aaa712c257ad8785a
EELMMAD <elmiramadadi@gmail.com>
2022-07-22 22:16:15 +0200

    create MathStuff3.java comparing numbers

---
+++ src/main/java/se/udemy/elmira/Numbers/MathStuff3.java
@@ -0,0 +1,22 @@
+package se.udemy.elmira.Numbers;
+
+public class MathStuff3 {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 3;
+        System.out.println(num1 == num2);
+        System.out.println(num1 > num2);
+        System.out.println(num1 < num2);
+        System.out.println(num1 <= num2);
+        System.out.println(num1 <= num2);
+
+
+        System.out.println("-----------");
+        int num3 = 5;
+        int num4 = 9;
+        System.out.println(num3 == num4);
+        System.out.println(num3 > num4);
+        System.out.println(num3 < num4);
+        System.out.println(num3 != num4);
+    }
+}
