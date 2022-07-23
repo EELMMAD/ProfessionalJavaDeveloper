d6d86f1dc60a436a5e51166aa57186f50b4fc087
EELMMAD <elmiramadadi@gmail.com>
2022-07-23 08:50:46 +0200

    Introducing BigDecimal

---
+++ src/main/java/se/udemy/elmira/Numbers/NumberStuff3.java
@@ -0,0 +1,15 @@
+package se.udemy.elmira.Numbers;
+
+import java.math.BigDecimal;
+
+public class NumberStuff3 {
+    public static void main(String[] args) {
+        float num1 = 2.15f;
+        float num2 = 1.10f;
+        System.out.println(num1- num2);
+
+        BigDecimal num3 = new BigDecimal("2.15");
+        BigDecimal num4 = new BigDecimal("1.10");
+        System.out.println(num3.subtract(num4));
+    }
+}
