c571b23ff05e0b99d7ada853ed95674947f576ac
EELMMAD <elmiramadadi@gmail.com>
2022-07-24 23:05:32 +0200

    Formatting Numbers

--- src/main/java/se/udemy/elmira/Numbers/CompoundInterestCalc.java
+++ src/main/java/se/udemy/elmira/Numbers/CompoundInterestCalc.java
@@ -1,21 +1,31 @@
 package se.udemy.elmira.Numbers;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
-public class CompoundInterestCalc { //Calculating compound interest
-    public static BigDecimal calculate(String principle, String rate, int period, String contribution){
+public class CompoundInterestCalc {
+    //£36,075.81   -->£
+    private static final NumberFormat moneyFormatter = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormatter = NumberFormat.getPercentInstance();
+
+    public static BigDecimal calculate(String principle, String rate, int period, String contribution) throws ParseException {
        // Balance(Y) = P(1 + r)^Y + c[ ((1 + r)^Y - 1) / r]
-        BigDecimal a = BigDecimal.ONE.add(new BigDecimal(rate)).pow(period); //(1 + r) ^ period
-        BigDecimal c = a.subtract(BigDecimal.ONE); // ((1 + r)^Y - 1)
-        BigDecimal d = c.divide(new BigDecimal(rate)); // ((1 + r)^Y - 1) / r
-        BigDecimal e = d.multiply(new BigDecimal(contribution)); //c[ ((1 + r)^Y - 1) / r]
-        BigDecimal f = a.multiply(new BigDecimal(principle)); //(1 + r) ^ period * principle
-        BigDecimal g = f.add(e);
-        return g;
+            String rateAsPercent = percentFormatter.parse(rate).toString();
+            BigDecimal a = BigDecimal.ONE.add(new BigDecimal(rateAsPercent)).pow(period); //(1 + r) ^ period
+            BigDecimal c = a.subtract(BigDecimal.ONE); // ((1 + r)^Y - 1)
+            BigDecimal d = c.divide(new BigDecimal(rateAsPercent)); // ((1 + r)^Y - 1) / r
+            BigDecimal e = d.multiply(new BigDecimal(moneyFormatter.parse(contribution).toString())); //c[ ((1 + r)^Y - 1) / r]
+            BigDecimal f = a.multiply(new BigDecimal(moneyFormatter.parse(principle).toString())); //(1 + r) ^ period * principle
+            BigDecimal g = f.add(e);
+            return g;
     }
 
-    public static void main(String[] args) {
-        BigDecimal balance = CompoundInterestCalc.calculate("10000.00", ".08", 10, "1000");
-        System.out.println(balance);
+    public static void main(String[] args) throws ParseException {
+        // NumberFormat moneyFormatter = NumberFormat.getCurrencyInstance(Locale.FRANCE); // 36 075,81 €   -->€
+       // NumberFormat moneyFormatter = NumberFormat.getCurrencyInstance(Locale.JAPAN); // ￥36,076  -->￥
+        BigDecimal balance = CompoundInterestCalc.calculate("$25,300", "8%", 10, "$7,500");
+        System.out.println(moneyFormatter.format(balance));
     }
 }
