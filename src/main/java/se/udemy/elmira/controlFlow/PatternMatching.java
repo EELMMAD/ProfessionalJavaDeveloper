793b635c4b871793931fc129ab2cc1b8ffeae717
EELMMAD <elmiramadadi@gmail.com>
2022-08-05 21:15:40 +0200

    switch statement pattern matching JDK 17 preview

---
+++ src/main/java/se/udemy/elmira/controlFlow/PatternMatching.java
@@ -0,0 +1,25 @@
+package se.udemy.elmira.controlFlow;
+
+public class PatternMatching { //switch statement pattern matching JDK 17 preview
+    /*
+    public static void main(String[] args) {
+        record Person(String firstName, String lastName, int age){}  //record -> class
+        String var1 = "Hello World";
+        Integer var2 = 34;
+        String[] var3 = {"hello", "world"};
+        Person var4 = new Person("Jake", "Johnson", 40);
+        Person var5 = new Person("Abe", "Johnson", 40);
+
+        Object obj = 35d; //double ------> Have no idea
+        switch (obj){  //
+            case String msg -> System.out.println(msg);
+            case Integer num -> System.out.println("Your number is: " + num);
+            case Person p && p.firstNmae.lengh() > 3 -> system.out.printLn("Looks like Jake");
+            case String[] arr -> System.out.printf("Looks like an array with %d elements.");
+            case null -> System.out.println("It is null");
+             default -> System.out.println("Have no idea");
+        }
+    }
+
+     */
+}
