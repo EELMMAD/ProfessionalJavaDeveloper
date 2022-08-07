10faae978503310e91afe1a5a2731ab2574ea6e1
EELMMAD <elmiramadadi@gmail.com>
2022-08-07 14:59:21 +0200

    simple for loop

---
+++ src/main/java/se/udemy/elmira/controlFlow/forLoop/ForLoopExample.java
@@ -0,0 +1,10 @@
+package se.udemy.elmira.controlFlow.forLoop;
+
+public class ForLoopExample { //a simpler for loop
+    public static void main(String[] args) {
+        int counter;
+        for (counter = 0; ; ) {
+            System.out.printf("Counter is %d%n ", counter + 1);
+        }
+    }
+}
