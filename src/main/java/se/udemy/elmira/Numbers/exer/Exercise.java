4dca75f1cdf676dab13c205fdb496cf43812ac24
EELMMAD <elmiramadadi@gmail.com>
2022-08-08 22:31:19 +0200

    Initial commit

---
+++ src/main/java/se/udemy/elmira/Numbers/exer/Exercise.java
@@ -0,0 +1,10 @@
+package se.udemy.elmira.Numbers.exer;
+
+public class Exercise {
+        public static void main(String[] args) {
+            String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+            System.out.println(daysOfWeek.length); // Print # of item in array
+            System.out.println(daysOfWeek[3]); // Print 4th day in the array. Note: arrays are zero-based, so 1st item is 0 & 4th item is 3.
+        }
+    }
+
