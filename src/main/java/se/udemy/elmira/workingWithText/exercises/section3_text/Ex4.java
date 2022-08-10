aea4fce8981408bedd8ca71f7fcd7db52a89a5b6
EELMMAD <elmiramadadi@gmail.com>
2022-08-10 23:21:31 +0200

    Initial commit

---
+++ src/main/java/se/udemy/elmira/workingWithText/exercises/section3_text/Ex4.java
@@ -0,0 +1,24 @@
+package se.udemy.elmira.workingWithText.exercises.section3_text;
+
+public class Ex4 {
+    /*
+If someone enters “ alphabet “ (notice the spaces) as input into a method, make that
+method output “alphabeT” instead
+
+     */
+
+    /*
+    public static void main(String[] args) {
+        String input = "  alphabet ";
+        String strippedInput = input.strip();
+        String firstPart = strippedInput.substring(0, strippedInput.length() - 1);
+        Character lastLetter = strippedInput.charAt(strippedInput.length() - 1);
+        // or
+//      String lastLetter = strippedInput.substring(strippedInput.length() - 1);
+        System.out.println(firstPart + lastLetter.toString().toUpperCase());
+    }
+}
+}
+
+     */
+}
