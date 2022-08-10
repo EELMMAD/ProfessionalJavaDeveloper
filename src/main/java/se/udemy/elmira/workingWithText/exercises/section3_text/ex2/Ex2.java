a5e53a454c448634bdd5663280db52e23a398775
EELMMAD <elmiramadadi@gmail.com>
2022-08-10 23:21:16 +0200

    Initial commit

--- src/main/java/se/udemy/elmira/workingWithText/exercises/section3_text/ex2/Ex2.java
+++ src/main/java/se/udemy/elmira/workingWithText/exercises/section3_text/ex2/Ex2.java
@@ -1,4 +1,4 @@
-package se.udemy.elmira.workingWithText.exercises.section3_text;
+package se.udemy.elmira.workingWithText.exercises.section3_text.ex2;
 
 public class Ex2 {
     public static void main(String[] args) {
