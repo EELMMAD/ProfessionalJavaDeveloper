20e8f2e7e8b1ba0aa5b91e190d675220e8bdc76c
EELMMAD <elmiramadadi@gmail.com>
2022-08-13 22:34:09 +0200

    Initial commit

---
+++ src/main/java/se/udemy/elmira/Numbers/exercise/Ex2.java
@@ -0,0 +1,11 @@
+package se.udemy.elmira.Numbers.exercise;
+
+public class Ex2 {
+    /*
+    What’s an alternative way you could add 2 + 4 without using “+” symbol?
+     */
+    public static void main(String[] args) {
+        System.out.println(2 | 4);
+        System.out.println(2 + 4);
+    }
+}
