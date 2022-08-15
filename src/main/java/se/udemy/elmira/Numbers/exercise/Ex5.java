01fda7077271ce9b2f1d22fdc2b72717d1ac9c78
EELMMAD <elmiramadadi@gmail.com>
2022-08-15 18:51:51 +0200

    Initial commit

---
+++ src/main/java/se/udemy/elmira/Numbers/exercise/Ex5.java
@@ -0,0 +1,33 @@
+package se.udemy.elmira.Numbers.exercise;
+
+import java.util.Random;
+
+public class Ex5 {
+    /*
+        . Make a method, next(), that can be called 10 times and generate a random integer between
+    0 & 10 (non-inclusive). This method keep a running sum of all random numbers it generates
+    and return that sum each time. If first time next() is called, it generates 5, and second time
+    it’s called it generates 3, it should return 8 from the second call - for example.
+     */
+
+    private int sum = 0;
+
+    public int next() {
+        this.sum += new Random().nextInt(10);
+        return this.sum;
+    }
+
+    public static void main(String[] args) {
+        Ex5 exercise5 = new Ex5();
+        System.out.println(exercise5.next());
+        System.out.println(exercise5.next());
+        System.out.println(exercise5.next());
+        System.out.println(exercise5.next());
+        System.out.println(exercise5.next());
+        System.out.println(exercise5.next());
+        System.out.println(exercise5.next());
+        System.out.println(exercise5.next());
+        System.out.println(exercise5.next());
+        System.out.println(exercise5.next());
+    }
+}
